c02390f1f5479bd1e6237a0925c8c0d71b85f415
Yusuke Yoshimoto <immortal836@gmail.com>
2015-07-06 23:41:13 +0900

    handle arithmetic and logical expressions

--- app/controllers/parsing.scala
+++ app/controllers/parsing.scala
@@ -65,51 +65,91 @@ object Implicits {
 }
 
 object FLParser extends RegexParsers {
-  def const: Parser[Ast] = """[0-9]+|true|false""".r ^^ { Const(_) }
-  val reserved = Set("true", "false", "nil", "if", "then", "else",
-                     "let", "in", "case", "of")
-  def variable: Parser[Var] = new Parser[Var] {
-    def apply(in: Input) = {
-      regex("""[a-zA-Z][a-zA-Z0-9_]*""".r)(in) match {
-        case Success(varName, next) =>
-          if (reserved(varName))
-            new Failure("variable expected but reserved word `" +
-                        varName + "' found", in)
-          else
-            Success(Var(varName), next)
-        case NoSuccess(msg, next) => Failure(msg, next)
+  // 入力文字列全体を一つの式だと思ってパース
+  def parse(input: String) = parseAll(expr, input)
+  def expr: Parser[Ast] = consExpr
+
+  def consExpr: Parser[Ast] = rep1sep(orExpr, "::") ^^ { results =>
+    results.reduceRight(Cons(_,_))
+  }
+
+  def binOpExpr(operator: Parser[String], nextLevel: Parser[Ast]): Parser[Ast] =
+    nextLevel~rep(operator~nextLevel) ^^ { case first~rest =>
+      rest.foldLeft(first){ (tree, node) =>
+        node match {
+          case op~expr => App(App(Const(op), tree), expr)
+        }
+      }
+    }
+
+  // 識別子または予約語
+  def word: Parser[String] = """[a-zA-Z][a-zA-Z0-9_]*""".r
+  // 述語predを満たすword
+  // wordが見つかったがpredを満たさない場合、err(word)がエラーメッセージとなる
+  def wordWith(pred: String => Boolean, err: String => String): Parser[String] =
+    new Parser[String] {
+      def apply(in: Input) = {
+        word(in) match {
+          case Success(w, next) =>
+            if (pred(w))
+              Success(w, next)
+            else
+              Failure(err(w), in)
+          case NoSuccess(msg, next) => Failure(msg, next)
+        }
+      }
+    }
+  // errを省略したwordWith
+  def wordWith(pred: String => Boolean): Parser[String] =
+    wordWith(pred, "`" + _ + "' does not satisfy the required condition")
+  // 与えられた文字列群のどれか一つに一致するword
+  def word(expecteds: String*): Parser[String] = wordWith(expecteds.contains(_))
+
+  def orExpr: Parser[Ast] = binOpExpr(word("or"), andExpr)
+  def andExpr: Parser[Ast] = binOpExpr(word("and"), comparisonExpr)
+  def comparisonExpr: Parser[Ast] = binOpExpr("<=|<|=|>=|>".r, addsubExpr)
+  def addsubExpr: Parser[Ast] = binOpExpr("""\+|-""".r, muldivExpr)
+  def muldivExpr: Parser[Ast] = binOpExpr("""\*|/""".r, prefixExpr)
+
+  def prefixExpr: Parser[Ast] = {
+    val op = "+" | "-" | word("not")
+    opt(op)~appExpr ^^ {
+      case prefix~expr => prefix match {
+        case Some(op) => App(Const(op), expr)
+        case None => expr
       }
     }
   }
+
+  def appExpr: Parser[Ast] = rep1(parenExpr) ^^ { results =>
+    results.reduceLeft(App(_,_))
+  }
+  def parenExpr: Parser[Ast] = nonLeftRecursiveExpr | "("~>expr<~")"
+
+  def nonLeftRecursiveExpr: Parser[Ast] =
+    const | variable | nil | abs | ifExpr | letExpr | caseExpr
+
+  def const: Parser[Ast] =
+    ("""[0-9]+""".r | word("true", "false")) ^^ { Const(_) }
+
+  val reservedWords = Set("true", "false", "and", "or", "nil",
+                          "if", "then", "else", "let", "in", "case", "of")
+  def variable: Parser[Var] = wordWith(!reservedWords(_)) ^^ { Var(_) }
+
   def nil: Parser[Nil.type] = "nil" ^^ { _ => Nil }
-  def abs: Parser[Abs] = "\\"~>variable~"."~expr ^^ {
+  def abs: Parser[Abs] = """\\|λ""".r~>variable~"."~expr ^^ {
     case variable~sep~body => Abs(variable, body)
   }
   def ifExpr: Parser[If] = "if"~>expr~"then"~expr~"else"~expr ^^ {
-    case cond~sep1~thenExpr~sep2~elseExpr => If(cond, thenExpr, elseExpr)
-      }
-  def let: Parser[Let] = "let"~>variable~"="~expr~"in"~expr ^^ {
-    case variable~sep1~binding~sep2~body => Let(variable, binding, body)
+    case cond~_~thenExpr~_~elseExpr => If(cond, thenExpr, elseExpr)
+  }
+  def letExpr: Parser[Let] = "let"~>variable~"="~expr~"in"~expr ^^ {
+    case variable~_~binding~_~body => Let(variable, binding, body)
   }
   def caseExpr: Parser[Case] =
-    "case"~>expr~"of"~
-      opt("|")~"nil"~"->"~expr~
-      "|"~variable~"::"~variable~"->"~expr ^^ {
-        case e~_~_~_~_~nilE~_~v1~_~v2~_~consE =>
-        Case(e, nilE, v1, v2, consE)
-      }
-
-  def nonrecExpr: Parser[Ast] =
-    const | variable | nil | abs | ifExpr | let | caseExpr
-
-  def expr: Parser[Ast] = maybeCons
-  def maybeCons: Parser[Ast] = rep1sep(maybeApp, "::") ^^ { results =>
-    results.reduceRight(Cons(_,_))
-  }
-  def maybeApp: Parser[Ast] = rep1(maybeParen) ^^ { results =>
-    results.reduceLeft(App(_,_))
-  }
-  def maybeParen: Parser[Ast] = nonrecExpr | "("~>expr<~")"
-
-  def parse(input: String) = parseAll(expr, input)
+    "case"~>expr~"of" ~ opt("|")~"nil"~"->"~expr~
+    "|"~variable~"::"~variable~"->"~expr ^^ {
+      case e~_~_~_~_~nilE~_~v1~_~v2~_~consE =>
+      Case(e, nilE, v1, v2, consE)
+    }
 }
--- test/ParsingTest.scala
+++ test/ParsingTest.scala
@@ -5,9 +5,82 @@ import org.scalatest.FunSuite
 class ParsingTest extends FunSuite {
   import parsing.Implicits._
 
+  test("'true'") {
+    assertResult(Const("true")) { FLParser.parse("true").get }
+  }
+
+  test("lambda abstraction") {
+    assertResult(Abs("x", Abs("y", App("x", "y")))) {
+      FLParser.parse("\\x.λy.x y").get
+    }
+  }
+
   test("case expression") {
     assertResult(Case(Cons(1,Nil), 0, "hd", "tl", "hd")) {
       FLParser.parse("case 1::nil of nil -> 0 | hd::tl -> hd").get
+    }
+  }
+
+  test("simple arithmetic expression") {
+    assertResult(App(App(Const("-"), App(App(Const("+"), 1), 2)), 3)) {
+      FLParser.parse("1+2-3").get
+    }
+  }
+
+  test("operator '<='") {
+    assertResult(App(App(Const("<="), 1), 2)) {
+      FLParser.parse("1<=2").get
+    }
+  }
+
+  test("method 'word'") {
+    assertResult("and") {
+      FLParser.parseAll(FLParser.word("and"), "and").get
+    }
+  }
+
+  test("operator 'and'") {
+    assertResult(App(App(Const("and"), true), false)) {
+      FLParser.parse("true and false").get
+    }
+  }
+
+  test("operator 'not'") {
+    assertResult(App(Const("not"), true)) {
+      FLParser.parse("not true").get
+    }
+  }
+
+  test("logical expression") {
+    assertResult(App(App(Const("or"), App(Const("not"), true)),
+                     App(App(Const("and"), true), false))) {
+      FLParser.parse("not true or true and false").get
+    }
+  }
+
+  test("complex arighmetic and logical expression 1") {
+    assertResult(App(App(Const("and"),
+                         App(App(Const("<="), 1), 2)),
+                     App(App(Const("<"), 3), 4))) {
+      FLParser.parse("1<=2 and 3<4").get
+    }
+  }
+
+  test("complex arithmetic and logical expression 2") {
+    assertResult(App(App(Const("and"),
+                         App(App(Const("<="),
+                                 App(App(Const("+"), 1), 2)),
+                             3)),
+                     App(App(Const("<"),
+                             App(Const("-"), 4)),
+                         5))) {
+      FLParser.parse("1+2<=3and-4<5").get
+    }
+  }
+
+  test("character '=' as operator and let binder") {
+    assertResult(Let("x", App(App(Const("="), 1), 2), App(Const("not"), "x"))) {
+      FLParser.parse("let x=1=2 in not x").get
     }
   }
 }
