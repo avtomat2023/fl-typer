3da1941db59cf46a982d54910905d00068ed39b6
Yusuke Yoshimoto <immortal836@gmail.com>
2015-07-12 14:13:08 +0900

    fix a bug on type button disabling

--- public/javascripts/typing.js
+++ public/javascripts/typing.js
@@ -234,12 +234,12 @@ jQuery(function($) {
             // 送信前
             beforeSend: function(xhr, settings) {
                 // ボタンを無効化し、二重送信を防止
-                $(this).attr('disabled', true);
+                $('#type-button').attr('disabled', true);
             },
             // 応答後
             complete: function(xhr, textStatus) {
                 // ボタンを有効化し、再送信を許可
-                $(this).attr('disabled', false);
+                $('#type-button').attr('disabled', false);
             },
 
             // 通信成功時の処理
